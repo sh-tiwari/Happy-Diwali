8653519b3b7ba0a4168e964308638f9aece0fdab
Shashwat Tiwari <shashwattiwari.0508@gmail.com>
2025-10-14 06:40:04 +0000

    removed intro

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
 if st.button("Send balloons!"):
     st.balloons()
